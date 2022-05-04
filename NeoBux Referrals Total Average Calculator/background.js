65720a442cd5e734fb56ca7c5670dc894fe7f7f8
Axolotl2 <idanedri94@gmail.com>
2022-05-04 09:30:31 +0300

    Expand logic

--- NeoBux Referrals Total Average Calculator/background.js
+++ NeoBux Referrals Total Average Calculator/background.js
@@ -13,22 +13,33 @@
 //    }  
 //}
 
-function showReferralsAverage() {
+function getReferralsData() {
 	var table = document.getElementById("mnTb");
 	var sum = 0.0;
 	var referrals = 0;
 	var lastRow;
+	var totalProfit = 0.0;
+	var estimatedProfit = 0.0;
 	for (var i = 0, row; row = table.rows[i]; i++) {
 		if ( row.className.includes("t") ){
-			sum += parseFloat(row.cells[7].innerText);
+			var clicks = parseFloat(row.cells[6].innerText)
+			var averageClicks = parseFloat(row.cells[7].innerText);
+			var daysRegex = /[^\s]*(?=\sdays)/g;
+			var days = parseFloat(row.cells[4].innerText.match(daysRegex)[0]);
+			sum += averageClicks;
+			console.log("i: " + i + "clicks: " + clicks + " days: " + days);
+			totalProfit += 0.005 * clicks;
+			estimatedProfit += days * 0.005 * averageClicks;
 			referrals++;
 		} 
 		lastRow = row;
 	}
-	var totalAverage = (sum / referrals);
-	var outputText = "The total average for " + referrals + " referrals Is: " + totalAverage.toFixed(2);
-	lastRow.cells[0].innerText = outputText;
-	
+	var totalAverage = sum / referrals;
+
+    return { referrals: referrals, totalAverage: totalAverage, dataRow: lastRow, totalProfit: totalProfit, estimatedProfit: estimatedProfit};
+}
+
+function paintRow(lastRow, totalAverage) {
 	var min = document.getElementById("out1").value;
 	var max = document.getElementById("out2").value;
 	lastRow.cells[0].className = '';
@@ -40,6 +51,16 @@ function showReferralsAverage() {
 	} else {
 	lastRow.cells[0].bgColor = 'yellow';
 	}
+}
+
+function showReferralsAverage() {
+	
+	var refData = getReferralsData();
+	var outputText = "The total average for " + refData.referrals + " referrals Is: " + refData.totalAverage.toFixed(2) + "\ncurrent profit: " + refData.totalProfit.toFixed(2) + "$ estimated additional profit till end of lease: " + refData.estimatedProfit.toFixed(2) + "$";
+	refData.dataRow.cells[0].innerText = outputText;
+	
+	paintRow(refData.dataRow, refData.totalAverage);
+
 }
 
 chrome.action.onClicked.addListener((tab) => {
